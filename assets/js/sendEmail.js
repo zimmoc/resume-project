1956e5312e3843e78409ecdd496caba0e4b2c66f
zimmoc <128923890+zimmoc@users.noreply.github.com>
2024-03-14 23:57:41 +0100

    Add confirmation message to contact form submit

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -6,7 +6,14 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
+            const success = document.createElement("div");
+            const newContent = document.createTextNode('Form successfully submitted!');
+            success.appendChild(newContent);
+            $(success).addClass('alert alert-success');
+            $(success).attr('role', 'alert');
+
             console.log("SUCCESS", response);
+            document.getElementById("form-submit").replaceWith(success);
         },
         function(error) {
             console.log("FAILED", error);
