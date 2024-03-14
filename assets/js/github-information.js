8bb47ff0ce6ec067062a609771cdc3d207cdf284
zimmoc <128923890+zimmoc@users.noreply.github.com>
2024-03-14 22:24:49 +0100

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,8 +1,8 @@
 function userInformationHTML(user) {
-    return `
+  return `
         <h2>${user.name}
             <span class="small-name">
-                (@<a href="${user.html_url} target="_blank">${user.login}</a>)
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
@@ -14,9 +14,29 @@ function userInformationHTML(user) {
             <p>Followers: ${user.followers} - Following ${user.following} <br> 
                 Repos: ${user.public_repos}
             </p>
-        </div>`
+        </div>`;
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class"clearfix repo-list>No repos!</div>`
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`
+    })
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join('\n')}
+                </ul>
+            </div>`;
+}
 
 function fetchGitHubInformation(event) {
   var username = $('#gh-username').val();
@@ -31,10 +51,14 @@ function fetchGitHubInformation(event) {
             </div>`
   );
 
-  $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
-    function (response) {
-      var userData = response;
+  $.when($.getJSON(`https://api.github.com/users/${username}`),
+  $.getJSON(`https://api.github.com/users/${username}/repos`)
+  ).then(
+    function (firstResponse, secondResponse) {
+      var userData = firstResponse[0];
+      var repoData = secondResponse[0];
       $('#gh-user-data').html(userInformationHTML(userData));
+      $('#gh-repo-data').html(repoInformationHTML(repoData));
     },
     function (errorResponse) {
       if (errorResponse.status === 404) {
